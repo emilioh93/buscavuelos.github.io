2c70b06e23f28e6ff380c1a29d46854e957d2a2f
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-07-23 20:37:08 -0300

    agregué api de paises

---
+++ src/components/ConfigRegional.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { Fragment } from "react";
+import { Form, FloatingLabel } from "react-bootstrap";
+
+const ConfigRegional = () => {
+  const [arregloMonedas, setArregloMonedas] = useState([]);
+  const [arregloPaises, setArregloPaises] = useState([]);
+
+  let consultarMonedaURL =
+    "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/currencies";
+  let consultarPaisURL =
+    "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/countries/en-US";
+
+  useEffect(() => {
+    consultarMoneda();
+    consultarPais();
+  }, []);
+
+  const consultarMoneda = () => {
+    fetch(consultarMonedaURL, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "3a61d965efmsh763ab4a859eb191p1173b1jsn61d45064a77d",
+        "x-rapidapi-host":
+          "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+      },
+    })
+      .then((response) => response.json())
+      .then((datos) => {
+        console.log(datos.Currencies);
+        setArregloMonedas(datos.Currencies);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const consultarPais = () => {
+    fetch(consultarPaisURL, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "3a61d965efmsh763ab4a859eb191p1173b1jsn61d45064a77d",
+        "x-rapidapi-host":
+          "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+      },
+    })
+      .then((response) => response.json())
+      .then((datos) => {
+        console.log(datos.Countries);
+        setArregloPaises(datos.Countries);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  return (
+    <Fragment>
+      <FloatingLabel controlId="floatingSelect" label="Moneda">
+        <Form.Select id="contenedorMonedas" aria-label="Default select example">
+          {arregloMonedas &&
+            arregloMonedas.map((moneda) => {
+              return (
+                <option>
+                  {moneda.Code}-{moneda.Symbol}
+                </option>
+              );
+            })}
+        </Form.Select>
+      </FloatingLabel>
+      <FloatingLabel className="ms-3" controlId="floatingSelect" label="País/región">
+        <Form.Select id="contenedorPaises" aria-label="Default select example">
+          {arregloPaises &&
+            arregloPaises.map((pais) => {
+              return (
+                <option>
+                  {pais.Code}-{pais.Name}
+                </option>
+              );
+            })}
+        </Form.Select>
+      </FloatingLabel>
+    </Fragment>
+  );
+};
+
+export default ConfigRegional;
--- src/components/FormularioBusqueda.js
+++ src/components/FormularioBusqueda.js
@@ -1,62 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
 
 const FormularioBusqueda = () => {
-  //   const [moneda, setMoneda] = useState("");
-  const [arregloMonedas, setArregloMonedas] = useState([]);
-
-  let consultarMonedaURL =
-    "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/currencies";
-
-  useEffect(() => {
-    consultarMoneda();
-  }, []);
-
-  const consultarMoneda = () => {
-    fetch(consultarMonedaURL, {
-      method: "GET",
-      headers: {
-        "x-rapidapi-key": "3a61d965efmsh763ab4a859eb191p1173b1jsn61d45064a77d",
-        "x-rapidapi-host":
-          "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-      },
-    })
-      .then((response) => response.json())
-      .then((datos) => {
-        console.log(datos.Currencies);
-        setArregloMonedas(datos.Currencies);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  };
-
   return (
     <Container>
       <Form className="mt-5">
         <section className="row d-flex justify-content-center">
-          <div className="col-md-2 col-sm-12">
+          <div className="col-md-3 col-sm-12">
             <Form.Control type="text" placeholder="Destino" />
           </div>
-          <div className="col-md-2 col-sm-12">
+          <div className="col-md-3 col-sm-12">
             <Form.Control type="text" placeholder="Origen" />
           </div>
-          <div className="col-md-2 col-sm-12">
-            <Form.Select aria-label="Default select example">
-              {arregloMonedas &&
-                arregloMonedas.map((moneda) => {
-                  return (
-                    <option>
-                      {moneda.Symbol} {moneda.Code}
-                    </option>
-                  );
-                })}
-            </Form.Select>
-          </div>
-          <div className="col-md-2 col-sm-12">
+          <div className="col-md-3 col-sm-12">
             <Form.Control type="text" placeholder="Locale" />
           </div>
-          <div className="col-md-2 col-sm-12">
+          <div className="col-md-3 col-sm-12">
             <Button className="w-100">Buscar</Button>
           </div>
         </section>
